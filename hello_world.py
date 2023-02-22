708a5104dbed995a70b59b313dff90d0ad31068e
Kilian <kilian.kugge@tuni.fi>
2023-02-22 11:47:07 +0200

    Clean up file

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-
 def main():
     print("Hello World!")
     print("Hello from feature")
 
 if __name__ == "__main__":
     main()
-=======
-print("Hello world!")
->>>>>>> intermediate-materials/master
